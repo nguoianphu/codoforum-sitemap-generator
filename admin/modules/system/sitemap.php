6e70c87b362a7b3cb3007e139ce6d918f0792373
Nguoi An Phu <nguoianphu@gmail.com>
2015-08-13 11:34:30 +0700

    Fix the ping

--- admin/modules/system/sitemap.php
+++ admin/modules/system/sitemap.php
@@ -18,10 +18,7 @@ class sitemap {
 			$sitemapObject = new Crawler($_GET['site_url']);
 			$sitemapPath = ABSPATH . 'sitemap.xml';
 			$sitemapFile = $sitemapObject->createSitemap($sitemapPath);
-			$sitemapPing=  $sitemapObject->SendSiteMapUpdateIndicationPing(RURI.'sitemap.xml');
-
-            // session_write_close();
-            // ob_end_flush();
+			$sitemapFile = $sitemapObject->SendSiteMapUpdateIndicationPing($_GET['sitemap_url']);
             exit();
         }
     }
